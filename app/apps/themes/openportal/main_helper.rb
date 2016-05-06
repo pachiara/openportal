3924eb1c608258b72bca86a9052479957579b5ce
Pachi <chiara.palmieri@gmail.com>
2016-05-06 17:35:47 +0200

    test installazione

--- app/apps/themes/openportal/main_helper.rb
+++ app/apps/themes/openportal/main_helper.rb
@@ -8,7 +8,7 @@ module Themes::Openportal::MainHelper
     # Also, you can save your extra values added in admin/settings.html.erb
     # sample: theme.set_meta("my_key", params[:my_value])
   end
-  
+
     # return a list of options for "Recent items from " on site settings -> theme settings
   def openportal_first_list_select
     res = []
@@ -28,13 +28,13 @@ module Themes::Openportal::MainHelper
     group.add_field({"name"=>"Title Column Center", "slug"=>"title_icon_section_center"}, {field_key: "editor", translate: true, default_value: "User Experience Focused"})
     group.add_field({"name"=>"Column Center", "slug"=>"icon_section_center"}, {field_key: "editor", translate: true, default_value: "Mettere al centro l'esperienza d'uso dell'utilizzatore con in mente sempre accessibilità, usabilità e innovazione."})
     group.add_field({"name"=>"Title Column Right", "slug"=>"title_icon_section_right"}, {field_key: "editor", translate: true, default_value: "Increase security"})
-    group.add_field({"name"=>"Column Right", "slug"=>"icon_section_right"}, {field_key: "editor", translate: true, default_value: "La condivisione diventa un potente mezzo per aumentare la sicurezza delle applicazioni."})
+    group.add_field({"name"=>"Column Right", "slug"=>"icon_section_right"}, {field_key: "editor", translate: true, default_value: "La condivisione è un potente mezzo per aumentare la sicurezza delle applicazioni."})
 
     group = theme.add_field_group({name: "Footer", slug: "footer"})
     group.add_field({"name"=>"Column Left", "slug"=>"footer_left"}, {field_key: "editor", translate: true, default_value: "<h5>Lombardia Informatica</h5><p>Via T.Taramelli, 26 - 20124 MILANO<br>Tel 02 39331.1 - Fax 02 39331.250<br> Milano, Italia. </p>"})
     group.add_field({"name"=>"Column Center", "slug"=>"footer_center"}, {field_key: "editor", translate: true, default_value: "<h5>Links</h5> <p><a href='http://www.lispa.it'>Sito Lispa</a><br> <a href='https://www.linkedin.com/company/lombardia-informatica'>Linkedin</a><br> <a href='https://www.facebook.com/LombardiaInformatica'>Facebook</a></p>"})
     group.add_field({"name"=>"Column Right", "slug"=>"footer_right"}, {field_key: "editor", translate: true, default_value: "<h5>About</h5><p>OpenPortal è un Portale aperto <br> per lo sviluppo collaborativo e cooperativo <br> dei servizi software.</p>"})
-    
+
     unless theme.get_field_groups.where(slug: "fields").any?
       group = theme.add_field_group({name: "Main Settings", slug: "fields", description: ""})
       group.add_field({"name"=>"Background color", "slug"=>"bg_color"},{field_key: "colorpicker"})
